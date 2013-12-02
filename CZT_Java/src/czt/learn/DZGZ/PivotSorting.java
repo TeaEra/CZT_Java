9bc3afbea60f9682e8902afe1dc622e5120e1671
CHEN Zhengong <cllf08214@qq.com>
2013-12-02 20:56:26 +0800

    Finish prime number solution for SmartSorting123; ready for pivot sorting;

---
+++ CZT_Java/src/czt/learn/DZGZ/PivotSorting.java
@@ -0,0 +1,15 @@
+package czt.learn.DZGZ;
+
+public class PivotSorting {
+	
+	public static void pivotSort() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//
+	}
+
+}
