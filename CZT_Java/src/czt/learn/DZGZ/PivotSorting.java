bfc052141de431db28bf8e051fcc7a05b0ea2ef7
CHEN Zhengong <cllf08214@qq.com>
2013-12-04 23:59:09 +0800

    The 1st version of QuickSort;

--- CZT_Java/src/czt/learn/DZGZ/PivotSorting.java
+++ CZT_Java/src/czt/learn/DZGZ/PivotSorting.java
@@ -3,23 +3,58 @@ package czt.learn.DZGZ;
 import java.util.ArrayList;
 import java.util.List;
 
-import czt.learn.Util.Constants;
+import czt.learn.Util.StaticMethods;
 
 public class PivotSorting {
 	
-	public static void pivotSort() {
+	// PivotSort + PivotPartition = QuickSort
+	// Here, I choose the first element as the pivot;
+	// Similarly, the last element could also be considered as the pivot;
+	// Then
+	// TODO: randomized QuickSort
+	// which means to choose pivot randomly;
+	
+	public static void pivotSort(int[] arr, int start, int end) {
+		if (start < end) {
+			int mid = pivotPartition(arr, start, end);
+			PivotSorting.pivotSort(arr, start, mid-1);
+			PivotSorting.pivotSort(arr, mid+1, end);
+		}
+	}
+	
+	public static int pivotPartition(int[] arr, int start, int end) {
+		int pivot = arr[start];
+		int smallIdx = start-1;
 		
+		for (int i=start+1; i<=end; ++i) {
+			if (arr[i] <= pivot) {
+				int temp = arr[i];
+				smallIdx++;
+				arr[i] = arr[smallIdx];
+				arr[smallIdx] = temp;
+			}
+		}
+		smallIdx++;
+		System.out.println(smallIdx);
+		int temp = arr[smallIdx];
+		arr[smallIdx] = pivot;
+		arr[start] = temp;
+		
+		return smallIdx;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		// Manually initialize an array for 10 integers
+		int[] arr = new int[] {4,6,1,5,8,9,2,3,7,0};
+		int start = 0;
+		int end = arr.length - 1;
+		
 		//
-		System.out.print("How many input numbers: ");
-		int size = Constants.scanner.nextInt();
-		List<Integer> inList = new ArrayList<Integer>();
-		for (int i=0; i<size; ++i) {
-			inList.add(new Integer(Constants.scanner.nextInt()));
+		PivotSorting.pivotSort(arr, start, end);
+		for (int i=0; i<arr.length; ++i) {
+			System.out.print(i + "\t");
 		}
 	}
 
