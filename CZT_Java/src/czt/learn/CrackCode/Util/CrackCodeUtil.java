bcdb1b7983147baec96f9ad667c6d93af72b46a9
CHEN Zhengong <cllf08214@qq.com>
2014-04-14 22:26:18 +0000

    Add some scanning;
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch master
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    #	modified:   CZT_Java/bin/.gitignore
    #

--- CZT_Java/src/czt/learn/CrackCode/Util/CrackCodeUtil.java
+++ CZT_Java/src/czt/learn/CrackCode/Util/CrackCodeUtil.java
@@ -1,14 +1,33 @@
 package czt.learn.CrackCode.Util;
 
+import java.io.*;
+import java.util.logging.Logger;
+
 /**
  * Created by: TeaEra;
  * Created date: 2014/4/6;
  */
 public class CrackCodeUtil {
 
+    /** File name to store prime numbers */
+    private final static String FILE_PRIME_NUMBERS = "PrimeNumbers.txt";
+
     //
+    public static void getPrimeNumbersToFile() {
+        try {
+            BufferedWriter bufferedWriter =
+                    new BufferedWriter(new OutputStreamWriter(new FileOutputStream(FILE_PRIME_NUMBERS), "utf-8"));
+        } catch (UnsupportedEncodingException e) {
+            // TODO
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            // TODO
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         //
+
     }
 }
